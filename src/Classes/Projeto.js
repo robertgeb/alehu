aecaf2145919e5845adffe892030d0088e4c9000
root <root@eduardo.eduardo>
2017-12-02 18:29:21 -0200

    criada a função para exibir a tabela de horas a trabalhar

--- src/Classes/Projeto.js
+++ src/Classes/Projeto.js
@@ -55,15 +55,58 @@ class Projeto {
 	}
 
 	printHorasTrabalhadas(){
-		let tabela = "------------------ Horas Trabalhadas ---------------------\n";
-		
-		for(let funcionario in this.funcionarios){
-			tabela += "|".concat(this.funcionarios[funcionario].getNome());
+		let tabela = "------------------ Horas Trabalhadas ---------------------\n",
+		    linhas_fases = [],
+		    horas_fases = {},
+		    fases_offset = 0,
+		    column_size = 10;
+
+		for(let fase in this.fases){
+			let nome_fase = this.fases[fase].getNome();
+			if(nome_fase.length > fases_offset)
+				fases_offset = nome_fase.length;
+
+			horas_fases[this.fases[fase].getNome()] = [];
 		}
 
-		tabela.concat("\n");
+		tabela += Array(fases_offset+1).join(" ");
 
-		
+		for(let idx_func in this.funcionarios){
+			let funcionario = this.funcionarios[idx_func],
+				offsets_len = Math.ceil((column_size - idx_func.toString().length) / 2),
+				offsets = Array(offsets_len).join(" ");
+
+			tabela += "|".concat((offsets / 2 == 0 ? offsets : offsets+1), "F", idx_func, offsets);
+			
+			for(let fase in horas_fases){
+				horas_fases[fase].push( funcionario.getMapaHorasTrabalhadas().getHorasByFase(fase) );
+			}
+
+		}
+
+		tabela += "\n";
+
+		for(let fase in horas_fases){
+			let offset_len = fases_offset - fase.length,
+				linha_fase = fase;
+
+			if(offset_len > 0){
+				linha_fase = fase.concat(Array(offset_len+1).join(" "));
+			}
+
+			for(let i=0; i<horas_fases[fase].length; i++){
+
+				let hora_fase = horas_fases[fase][i],
+					offsets_len = Math.ceil((column_size - hora_fase.toString().length) / 2),
+					offset_left = (offsets_len % 2) == 0 ? Array(offsets_len+1).join(" ") : Array(offsets_len+1).join(" "),
+					offset_right = (offsets_len % 2) == 0 ? Array(offsets_len+1).join(" ") : Array(offsets_len).join(" ");
+
+				linha_fase += "|".concat(offset_left, hora_fase, offset_right);
+			}
+
+			tabela += linha_fase.concat("\n");
+		}
+
 		console.log(tabela);
 	}
 
